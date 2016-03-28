e6714b62ce12c5dd24135f74ce8fc31394c644cf
pinethee <pinethee@naver.com>
2016-03-28 14:02:15 +0900

    3/28 실습

--- lab/0321/src/IOlab.ino
+++ lab/0321/src/IOlab.ino
@@ -1,26 +1,62 @@
-int swPin = 2;
-int ledPin = 13;
-int cnt = 0;
+// 3월 28일
+int potValue = 0;
+const int MAX = 1023;
+int ledSink = 9; 
+int ledDrive = 8;
+
+
+int State = 4, Next = 4;
 
 void setup(){
-    pinMode(swPin, INPUT);
-    pinMode(ledPin, OUTPUT);
+    pinMode(ledSink, OUTPUT);
+    pinMode(ledSink, OUTPUT);
     Serial.begin(9600);
 }
+
 void loop(){
-    // sensing part
-    int swValue = digitalRead(swPin);
-    
-    // judge & action part -> forwarding
-    if (swValue == HIGH){
-        digitalWrite(ledPin, HIGH);
-        //Serial.println("HIGH");
-        cnt++; // 누른 시간동안 계속 카운트됨
+    //sensing
+    potValue = analogRead(A0);
+
+    // Action: forwording the pot value
+    //Serial.println(potValue);
+   
+    // judgement and classification
+    if(potValue < MAX/4){
+        Next = 0;
+    }else if(potValue < MAX/2){
+        Next = 1;
+    }else if(potValue < MAX*3/4){
+        Next = 2;
     }else{
-        digitalWrite(ledPin, LOW);
-        //Serial.println("LOW");
+        Next = 3;
     }
-    Serial.println(cnt);
-    Serial.println(swValue);
-    delay(200);
+
+    if (State != Next){
+        State = Next;
+        Serial.print(potValue);
+        switch (State){
+            case 0:
+                digitalWrite(ledDrive, LOW);
+                digitalWrite(ledSink, HIGH);
+                Serial.println(" A");
+                break;
+            case 1:
+                digitalWrite(ledDrive, LOW);
+                digitalWrite(ledSink, LOW);
+                Serial.println(" B");
+                break;
+            case 2:
+                digitalWrite(ledDrive, HIGH);
+                digitalWrite(ledSink, HIGH);
+                Serial.println(" C");
+                break;
+            case 3:
+                digitalWrite(ledDrive, HIGH);
+                digitalWrite(ledSink, LOW);
+                Serial.println(" D");
+                break;
+        }
+    }
+    
+    delay(30); /// for our perception
 }
